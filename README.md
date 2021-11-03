da4aace68a9afd9bf8ea832315180c7e71e38dcb
planoiilolibuju <planoiilolibuju@gitlab.com>
2021-11-03 21:21:07 +0700

    initialize

---
+++ README.md
@@ -0,0 +1,5 @@
+# Parcelv2 React Starter
+
+ReactJS stater boilerplate for parcel version 2
+
+# Getting Started
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div  className="greeting">
+            <h1>Hello React from Parcel2-react-starter</h1>
+        </div>
+    )
+}
+
+export default App
